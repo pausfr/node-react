6fb94436b8735473b99a8503a12423971efe1d55
pausfr <lost7l@icloud.com>
2022-09-08 12:49:47 +0900

    집가자

--- boilerplate-mern-stack-master/client/src/components/views/MovieDetail/Sections/Favorite.js
+++ boilerplate-mern-stack-master/client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
 function Favorite(props) {
@@ -8,24 +8,62 @@ function Favorite(props) {
   const moviePost = props.movieInfo.backdrop_path;
   const movieRunTime = props.movieInfo.runTime;
 
-  useEffect(() => {
-    let variables = {
-      userFrom,
-      movieId,
-    };
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
 
+  let variables = {
+    userFrom: userFrom,
+    movieId: movieId,
+    movieTitle: movieTitle,
+    moviePost: moviePost,
+    movieRunTime: movieRunTime,
+  };
+
+  useEffect(() => {
     Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
       if (response.data.success) {
         console.log(response.data);
+        setFavoriteNumber(response.data.favoriteNumber);
       } else {
         alert("숫자 정보를 가져오는데 실패 했습니다.");
       }
     });
+
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setFavorited(response.data.favorited);
+      } else {
+        alert("정보를 가져오는데 실패 했습니다.");
+      }
+    });
   });
+
+  const onClickFavorite = () => {
+    if (Favorited) {
+      Axios.post("/api/favorite/removeFromFavorite", variables).then(
+        (response) => {
+          if (response.data.success) {
+          } else {
+            alert("Favorite 리스트에서 지우는 걸 실패했어요");
+          }
+        }
+      );
+    } else {
+      Axios.post("/api/favorite/addToFavorite", variables).then((response) => {
+        if (response.data.success) {
+        } else {
+          alert("Favorite 리스트에 추가 하는 걸 실패했어요");
+        }
+      });
+    }
+  };
 
   return (
     <div>
-      <button>Favorite</button>
+      <button onClick={onClickFavorite}>
+        {Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}
+      </button>
     </div>
   );
 }
--- boilerplate-mern-stack-master/server/routes/favorite.js
+++ boilerplate-mern-stack-master/server/routes/favorite.js
@@ -13,4 +13,31 @@ router.post("/favoriteNumber", (req, res) => {
   });
 });
 
+router.post("/favorited", (req, res) => {
+  req.body.movieId;
+
+  // 내가 이 영화를 디비에 넣엇는지 db에서 확인
+  Favorite.find({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+
+    let result = false;
+    if (info.length !== 0) {
+      result = true;
+    }
+    res.status(200).json({ success: true, favorited: result });
+  });
+});
+
+router.post("/removeFromFavorite", (req, res) => {});
+
+router.post("/addToFavorite", (req, res) => {
+    const favorite = new Favorite(req.body);
+    favorite.save((err, doc) => {
+        if(err) return res.status(400).
+    })
+});
+
 module.exports = router;
